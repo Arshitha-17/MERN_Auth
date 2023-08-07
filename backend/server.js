4eecef9a732ae12b445c75eb7e688cd29fe68591
Arshitha-17 <arshithaachu165@gmail.com>
2023-08-07 12:17:36 +0530

    complete update user profile

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from 'dotenv';
 import {notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+
 import connectDB from "./config/db.js";
 dotenv.config();
 const port= 8000;
 import userRoutes from './routes/userRoutes.js';
 
 const app= express();
+app.use(cookieParser()); // Place cookieParser here
 
 connectDB();
 
@@ -21,6 +23,6 @@ app.get('/',(req,res)=>res.send('server ready'))
 app.use(notFound)
 app.use(errorHandler)
 
-app.use(cookieParser());
+
 
 app.listen(port,()=>{ console.log('server started.....');})
