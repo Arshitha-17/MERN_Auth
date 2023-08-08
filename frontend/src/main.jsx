3a36653dd46535d494c6d3d50391166f3f72d369
Arshitha-17 <arshithaachu165@gmail.com>
2023-08-08 18:10:16 +0530

    create store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import store from './store.js';
+import { Provider } from 'react-redux';
 import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeScreen from './screens/HomeScreen.jsx';
@@ -17,7 +19,10 @@ const router= createBrowserRouter(
   )
   )
   ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+
   <React.StrictMode>
   <RouterProvider router={router} />
   </React.StrictMode>,
+    </Provider>
 )
