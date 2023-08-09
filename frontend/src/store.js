c78f8cb70167e09a89f903be62a632debcb2c5c6
Arshitha-17 <arshithaachu165@gmail.com>
2023-08-09 14:20:03 +0530

    logout complete

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './slices/authSlice';
+import { apiSlice } from './slices/apiSlice';
 
-const store= configureStore({
-    reducer: {},
-    middleware: (getDefaultMiddleware)=> getDefaultMiddleware(),
-    devTools:true
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    [apiSlice.reducerPath]:apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
-export default store; 
+export default store;
